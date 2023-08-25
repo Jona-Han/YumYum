7423525cccb000c17559f2c578280ab5058f726d
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2023-08-24 18:15:42 -0700

    Add MEDIA_IN_POSTS table to track the media i a post

---
+++ src/services/postService.ts
@@ -0,0 +1,42 @@
+import pool from '../utils/connectDB';
+
+export const getXRandomPosts = async (count: Number) => {
+  const result = await pool.query('SELECT * FROM POSTS ORDER BY RANDOM() LIMIT $1', [count]);
+  if (result.rows.length === 0) {
+    throw new Error('No posts found');
+  }
+  return result.rows;
+};
+
+export const getUserPosts = async (userID: String) => {
+  const result = await pool.query('SELECT * FROM POSTS WHERE user_ID = $1', [userID]);
+  return result.rows;
+};
+
+export const getSpecificPost = async (postID: Number) => {
+  const result = await pool.query('SELECT * FROM POSTS WHERE id = $1', [postID]);
+  return result.rows[0];
+};
+
+interface Post {
+  content: String;
+  category: Number;
+  userID: String;
+}
+
+export const createPost = async (userID: Number, post: Post) => {
+  const result = await pool.query(
+    'INSERT INTO POSTS(content, category, user_ID) VALUES($1, $2, $3)',
+    [post.content, post.category, post.userID]
+  );
+};
+
+export const deletePost = async (postID: Number) => {
+  const result = await pool.query('DELETE FROM POSTS WHERE id = $1', [postID]);
+  if (result.rows.length === 0) {
+    throw new Error('Post not found');
+  }
+  return result.rows[0];
+};
+
+export const updatePost = async (postID: Number, )
