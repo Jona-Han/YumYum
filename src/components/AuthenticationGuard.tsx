e11a0300ec60f93a856a62298541a66c364bab24
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2023-08-30 16:18:45 -0600

    Fix props bug in Authentication guard

--- src/components/AuthenticationGuard.tsx
+++ src/components/AuthenticationGuard.tsx
@@ -1,8 +1,8 @@
 import { withAuthenticationRequired } from '@auth0/auth0-react';
 import PageLoader from './PageLoader';
 
-export default function AuthenticationGuard({ children }: any) {
-  const ProtectedComponent = withAuthenticationRequired(children, {
+export default function AuthenticationGuard({ component }: any) {
+  const ProtectedComponent = withAuthenticationRequired(component, {
     onRedirecting: () => (
       <div>
         <PageLoader />
