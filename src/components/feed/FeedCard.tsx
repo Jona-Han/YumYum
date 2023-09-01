c9581f5816e6cb03e54be03ff66f3340161e11cf
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2023-09-01 10:16:47 -0600

    Add placeholder images for 5 posts

--- src/components/feed/FeedCard.tsx
+++ src/components/feed/FeedCard.tsx
@@ -36,7 +36,7 @@ export default function FeedCard() {
         <AspectRatio maxW={'500px'} ratio={4 / 3} bg={'gray.100'} mx={-4} mb={4}>
           <Image
             src={
-              'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+              '../../public/1.png'
             }
             alt="Example"
           />
